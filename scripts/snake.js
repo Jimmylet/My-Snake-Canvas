035b57db4d95a47ec1f551a0c1a05c86cf917381
Jimmylet <jimmy.letecheur@student.hepl.be>
2016-05-26 17:54:57 +0200

    Create the structure

---
+++ scripts/snake.js
@@ -0,0 +1,23 @@
+/*
+ * Created by Jimmy Letecheur on 26/05/16.
+ */
+
+
+(function() {
+    "use strict";
+
+        var oCanvas = document.getElementById( "game" ),
+            oContext = oCanvas.getContext( "2d" );
+
+        // Settings for the game
+        var game = {};
+
+        // Settings for the Snake
+        var snake = {};
+
+        // Settings for the food
+        var food = {};
+
+
+
+})();
