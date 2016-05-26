3d594e0e64d8a22ad623140f0e1f96276aa98539
Jimmylet <jimmy.letecheur@student.hepl.be>
2016-05-26 18:59:25 +0200

    initialize game start with text

--- scripts/snake.js
+++ scripts/snake.js
@@ -10,7 +10,51 @@
             oContext = oCanvas.getContext( "2d" );
 
         // Settings for the game
-        var game = {};
+        var game = {
+
+            messageGameStart: "",
+            messageGameOver: null,
+            gameIsOver: true,
+            speed: 0,
+
+            // GAME IS START
+            "start": function(){
+                game.gameIsOver = false;
+                game.messageGameOver = null;
+            },
+
+            // GAME IS OVER
+            "stop": function(){
+                game.gameIsOver = true;
+                game.messageGameStart = null;
+                game.messageGameOver = "";
+            },
+
+            // MESSAGE GAME IS START
+            "drawMessageGameIsStarted": function() {
+                if ( game.messageGameStart !== null ) {
+                    oContext.fillStyle = "white";
+                    oContext.font = "28px helvetica, arial";
+                    oContext.textAlign = "center";
+                    oContext.fillText( "Commences avec espace", oCanvas.width / 2, oCanvas.height / 2 );
+                }
+            },
+
+            // MESSAGE GAME IS OVER
+            "drawMessageGameIsOver": function() {
+                if ( game.messageGameOver !== null ) {
+                    oContext.fillStyle = "white";
+                    oContext.font = "28px helvetica, arial";
+                    oContext.textAlign = "center";
+                    oContext.fillText( "Commences avec espace", oCanvas.width / 2, oCanvas.height / 2 );
+                }
+            },
+
+            "resetGame": function() {
+                oContext.clearRect( 0, 0, oCanvas.width, oCanvas.height );
+            }
+
+        };
 
         // Settings for the Snake
         var snake = {};
@@ -18,6 +62,24 @@
         // Settings for the food
         var food = {};
 
+
+        var requestAnimationFrame = window.requestAnimationFrame;
+
+        function loadSettings() {
+            if ( game.over === false ) {
+                game.resetGame();
+                game.drawMessageGameIsOver();
+            } else {
+                game.drawMessageGameIsStarted();
+            }
+
+            setTimeout(function() {
+                requestAnimationFrame(loadSettings);
+            }, 1000 / game.speed );
+
+        }
+
+        requestAnimationFrame(loadSettings());
 
 
 })();
